b83f1749494eec636395981291586c496a0c1380
sirulab <chensiru.sjtu@gmail.com>
2026-08-05 17:52:14 +0800

    [feat] load data from json to sql

--- data/etl.py
+++ data/etl.py
@@ -11,25 +11,23 @@ conn = mysql.connector.connect(
     database="taipei_day_trip"
 )
 
-# IF NOT EX IST S | ?每個都 NOT NULL | ?不要事後改 | # RowNumber | info_date 或 build_date
+# IF NOT EX IST S | ?每個都 NOT NULL | ?不要事後改 | info_date 或 build_date
 cursor.execute('''CREATE TABLE IF NOT EXISTS attraction (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     rate INT,
     direction TEXT,
     date DATE,
-    avBegin DATE,
-    idpt DATE,
-    MEMO_TIME VARCHAR(255),
     longitude DOUBLE,
-    latitude DOUBLE,    
-    MRT VARCHAR(100),
-    SERIAL_NO VARCHAR(255),
-    CAT VARCHAR(255),
-    POI VARCHAR(10),
+    latitude DOUBLE,
+    mrt VARCHAR(100),
+    serial_no VARCHAR(100),
+    cat VARCHAR(100),
+    memo_time VARCHAR(255),
     description TEXT,
-    langinfo VARCHAR(10),
-    REF_WP''') 
+    address TEXT,
+    imgurls TEXT
+    ''') 
 
 #  ON DELETE CASCADE # AUTO_INCREMENT
 cursor.execute('''CREATE TABLE IF NOT EXISTS image (
@@ -41,3 +39,35 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS image (
 
 with open('taipei-attractions', 'r', encoding ='utf-8') as file:
     data_dict = json.load(file)
+
+attractions = data.get("list", []) # default 避免錯誤
+
+for attr in attractions:
+    attr_id = attr.get("_id")
+    sql_attr = """
+        INSERT INTO attraction 
+        (id, name, rate, direction, date, longitude, latitude, mrt, serial_no, cat, memo_time, description, address, imgurls)
+        VALUES (%s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s,  %s, %s)
+        """
+
+    val_attr = (
+        attr_id,
+        attr.get("name"),
+        attr.get("rate"),
+        attr.get("direction"),
+        attr.get("date"),
+        attr.get("longitude"),
+        attr.get("latitude"),
+        attr.get("MRT"),
+        attr.get("SERIAL_NO"),
+        attr.get("CAT"),
+        attr.get("MEMO_TIME"),
+        attr.get("description"),
+        attr.get("address"),
+        attr.get("imgurls")
+        )
+    
+    cursor.execute(sql_attr, val_attr)
+
+    ###
+    imgurls_raw = attr.get("imgurls", "")
